092646fec5dd76a845b5fde5f0533061156a7cc8
jeonbyeongmin <qudals7613@gmail.com>
2023-07-12 17:44:00 +0900

    build: add build script

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,3 +1,27 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2023 Byeongmin Jeon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import type { ModuleFormat, OutputOptions, RollupOptions } from 'rollup';
 
 import babel from '@rollup/plugin-babel';
@@ -5,41 +29,21 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import path from 'path';
 
-/**
- * config
- */
-
-const configs: RollupOptions[] = [
-  ...generateRollupConfig({
-    packageDir: 'packages/react',
-    entry: ['src/hooks/index.ts'],
-    external: ['react', 'react-dom'],
-  }),
-];
-
-export default configs;
-
-/**
- * helpers
- */
-
 type Options = {
   packageDir: string;
   entry: string | string[];
   external?: string[];
 };
 
-function generateBuilder(format: ModuleFormat): (opts: Options) => RollupOptions {
+function generateBuilder(format: ModuleFormat): (options: Options) => RollupOptions {
   const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-  return (opts: Options) => {
-    // input
-    const entries = Array.isArray(opts.entry) ? opts.entry : [opts.entry];
-    const input = entries.map(entry => path.resolve(opts.packageDir, entry));
+  return (options: Options) => {
+    const entries = Array.isArray(options.entry) ? options.entry : [options.entry];
+    const input = entries.map(entry => path.resolve(options.packageDir, entry));
 
-    // output
     const extension = format === 'esm' ? 'mjs' : 'js';
-    const outputFile = `${opts.packageDir}/build/lib/index.${extension}`;
+    const outputFile = `${options.packageDir}/build/lib/index.${extension}`;
     const output: OutputOptions = {
       format,
       file: outputFile,
@@ -47,8 +51,7 @@ function generateBuilder(format: ModuleFormat): (opts: Options) => RollupOptions
       sourcemap: true,
     };
 
-    // external
-    const external = opts.external || [];
+    const external = options.external || [];
 
     return {
       input,
@@ -69,9 +72,19 @@ function generateBuilder(format: ModuleFormat): (opts: Options) => RollupOptions
   };
 }
 
-function generateRollupConfig(opts: Options) {
-  const buildCJS = generateBuilder('cjs');
-  const buildESM = generateBuilder('esm');
+const buildCJS = generateBuilder('cjs');
+const buildESM = generateBuilder('esm');
 
-  return [buildCJS(opts), buildESM(opts)];
+function buildConfigs(options: Options) {
+  return [buildCJS(options), buildESM(options)];
 }
+
+const configs: RollupOptions[] = [
+  ...buildConfigs({
+    packageDir: 'packages/react',
+    entry: 'src/index.ts',
+    external: ['react', 'react-dom'],
+  }),
+];
+
+export default configs;
